67835b6e5ff2ed9808ae35ad32577301009013e2
Kurt Garcia <akod3vs@gmail.com>
2017-04-27 21:23:08 -0700

    working state

--- server/api/email/email.controller.js
+++ server/api/email/email.controller.js
@@ -1,76 +1,73 @@
 'use strict';
-
-var _ = require('lodash');
+ 
+var bunyan = require('bunyan');
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 // var User = require('../user/user.model');
 // var Roles = require('../roles/roles.model');
-
- var transporter = nodemailer.createTransport(smtpTransport( {
-	   service: "Gmail",  // sets automatically host, port and connection security settings
-	   auth: {
-	       user: "kurt.pricel06@gmail.com",
-	       pass: "@Kod3vS1992"
-	   }
-	}));
+  
  
-// exports.send = function(req, res) {
-//   console.log('hello');
-//   Roles.find({'permissions.receiveEmails': true}, function(err, foundRoles) {
-//     if(err) { return handleError(res, err); }
-//     if(!foundRoles) { return res.status(404).send('Nobody to email.'); }
 
-//     var gatheredRoles = [];
-//     for (var i = 0; i < foundRoles.length; i++) {
-//       gatheredRoles.push(foundRoles[i].role);
-//     }
+exports.testSend = function(req, res) {    
+ 
+	// Create a SMTP transporter object
+	var transporter = nodemailer.createTransport({
+	    service: 'Gmail',
+	    auth: {
+	       user: "kurt.pricel06@gmail.com",
+	       pass: "@Kod3vS1992" 
+	    },
+	    logger: bunyan.createLogger({
+	        name: 'nodemailer'
+	    }),
+	    debug: true // include SMTP traffic in the logs
+	}, {
+	    // default message fields
 
-//     User.find({role: {'$in': gatheredRoles}}, function (err2, emailees) {
-//       if(err2) { return handleError(res, err); }
-//       if(!emailees) { return res.status(404).send('Nobody to email.'); }
+	    // sender info
+	    from: 'KurtG <kurt.pricel06@gmail.com>',
+	    headers: {
+	        'X-Laziness-level': 1000 // just an example header, no need to use this
+	    }
+	});
+ 
+	console.log('SMTP Configured');
 
-//       var toEmails = [];
-//       for (var i = 0; i < emailees.length; i++) {
-//         toEmails.push(emailees[i].email);
-//       }
 
-//       var toos = toEmails.join();
-//       var mailOptions = {
-//         from: 'Fred Foo <foo@blurdybloop.com>', // sender address
-//         to: toos,
-//         subject: req.body.subject || '', // Subject line
-//         text: req.body.text || '', // plaintext body
-//       };
-//       res.status(200).send(mailOptions);
-//       // transporter.sendMail(mailOptions, function(error, info){
-//       //   if(error){ return handleError(res, err); }
-//       //   res.status(200).send('Email sent!');
-//       // });
-//     });
+	// Message object
+	var message = {
 
-//   });
+	    // Comma separated list of recipients
+	    to: 'Meshup <themeshstudio@gmail.com>',
 
-// };
+	    // Subject of the message
+	    subject: 'Nodemailer is unicode friendly ✔ #', //
 
-exports.testSend = function(req, res) {   
-	console.log(req);
-	var emailOptions = { 
-	   from: "SKurt Garcia <kurt.pricel06@gmail.com>", // sender address.  Must be the same as authenticated user if using Gmail.
-	   to: "Meshup <themeshstudio@gmail.com>",  
-	   subject: "Test mail nodemailer", 
-	   text: "Hi this is the test from the develoepr"  
+	    // plaintext body
+	    text: 'Hello to myself!',
+
+	    // HTML body
+	    html: '<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>' +
+	        '<p>Here\'s a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>',
+
+	    // Apple Watch specific HTML body
+	    watchHtml: '<b>Hello</b> to myself' 
+	   
 	};
 
-	transporter.sendMail(emailOptions, function(error, response){  
-	   if(error){
-	       console.log(error);
-	   } else {
-	       console.log("Message sent: " + response.message);
-	   }
-	   
-	   res.status(200).send(emailOptions);
-	   transporter.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
-	});  
+	console.log('Sending Mail');
+	transporter.sendMail(message, function(error, info){
+	    if (error) {
+	        console.log('Error occurred');
+	        console.log(error.message);
+	        return;
+	    }
+	    console.log('Message sent successfully!');
+	    console.log('Server responded with "%s"', info.response);
+	    transporter.close();
+	});
+	 
+
 };
 
 function handleError(res, err) {
